fc204d2e39eed02a0681415dcb4443a9c3cd87cd
Rainer Schoenberger <rschoe@de.ibm.com>
2019-03-07 16:02:23 +0100

    [#8] [#15] [#16] Ensure build and versioning works when run from code archive

--- src/generateVersionDefine.sh
+++ src/generateVersionDefine.sh
@@ -13,43 +13,75 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# This is a code-generator which attempts to generate a header file containing
+# version string. Following data sources are tried to get version information:
+# 1. git tags, commit hashes and status
+# 2. A version string in the environment variable GWHISPER_BUILD_VERSION if set.
+# 3. Build date and time
+# 4. Hard-coded version string HARD_CODED_VERSION found in this file.
+#
+# As an optional argument to this script a path containing the .git directory
+# for the gWhisper repository can be provided. This prevents git from searching
+# the FS tree for a git repository in case gWhisper is sub-packaged into another
+# repository
+
+# TODO: we could do this via git hooks. Problem is just that hooks are not
+#       really part of the repository. I.e. each user would need to install the
+#       hooks. Will need to find some solution
+HARD_CODED_VERSION="v0.1.5+"
+
 # get build date
 DATE=`date +%Y-%m-%d_%H:%M:%S`
 
+GITDIR=$1
+
+if [ "$GITDIR" = "" ]; then
+    GITCMD="git"
+else
+    cd $GITDIR
+    GITCMD="git --git-dir=.git"
+fi
+
+if [ "$GWHISPER_BUILD_VERSION" != "" ]; then
+    GWHISPER_BUILD_VERSION="Environment version:$GWHISPER_BUILD_VERSION "
+fi
 
 if [ -z "`command -v git`" ]; then
-    echo "$GWHISPER_BUILD_VERSION Build date: $DATE Build pwd: $PWD"
-    exit 0
+    versionString="${GWHISPER_BUILD_VERSION}Hard-coded version: $HARD_CODED_VERSION Build date: $DATE"
 fi
 
-git status &> /dev/null
+$GITCMD status &> /dev/null
 if [ $? -ne 0 ]; then
-    echo "$GWHISPER_BUILD_VERSION Build date: $DATE Build pwd: $PWD"
-    exit 0
+    versionString="${GWHISPER_BUILD_VERSION}Hard-coded version: $HARD_CODED_VERSION Build date: $DATE"
 fi
 
-# get unique readable commit identification
-gitId=$(git describe --tags)
+if [ "$versionString" = "" ]; then
+    # version string is still empty -> we can use git to generate the version string
 
-# check for uncommited changes
-uncommitedChanges=""
-git diff-index --quiet HEAD --
-if [ $? -eq 0 ]; then
-    uncommitedChanges="NoUncommitedChanges"
-else
-    uncommitedChanges="UncommitedChanges"
+    # get unique readable commit identification
+    gitId=$($GITCMD describe --tags)
+
+    # check for uncommited changes
+    uncommitedChanges=""
+    $GITCMD diff-index --quiet HEAD --
+    if [ $? -eq 0 ]; then
+        uncommitedChanges="NoUncommitedChanges"
+    else
+        uncommitedChanges="UncommitedChanges"
+    fi
+
+    # check for untracked files present
+    untrackedFiles=""
+    #tmpUntracked=$($GITCMD ls-files --other --directory --exclude-standard)
+    tmpUntracked=$($GITCMD status | grep "Untracked files")
+    if [ "$tmpUntracked" = "" ]; then
+        untrackedFiles="NoUntrackedFiles"
+    else
+        untrackedFiles="UntrackedFiles"
+    fi
+
+    versionString="${GWHISPER_BUILD_VERSION}Git identifier: ${gitId} Build date: ${DATE} ${uncommitedChanges} ${untrackedFiles}"
 fi
-
-# check for untracked files present
-untrackedFiles=""
-tmpUntracked=$(git ls-files --other --directory --exclude-standard)
-if [ -z "$tmpUntracked" ]; then
-    untrackedFiles="NoUntrackedFiles"
-else
-    untrackedFiles="UntrackedFiles"
-fi
-
-versionString="$GWHISPER_BUILD_VERSION Git identifier: ${gitId} Build date: ${DATE} ${uncommitedChanges} ${untrackedFiles}"
 
 echo "#pragma once"
 echo "#define GWHISPER_BUILD_VERSION \"$versionString\""
