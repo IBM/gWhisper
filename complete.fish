8e2a19e934ebb08c449b7e617c90cc8501bf6054
Rainer Schoenberger <rschoe@de.ibm.com>
2019-08-05 19:01:05 +0200

    [#64] clean up fish completion script
    Signed-off-by: Rainer Schoenberger <rschoe@de.ibm.com>

--- complete.fish
+++ complete.fish
@@ -1,18 +1,19 @@
-function getCompletions;
-  set commandLineUntilCursor (commandline -c)
-  set commandLineTokenized (commandline -o)
-  set commandName (commandline -o)[1]
+function _gwhisper_getCompletions;
+  set -l commandLineUntilCursor (commandline -c)
+  set -l commandLineTokenized (commandline -o)
+  set -l commandName (commandline -o)[1]
 
-  set args (string sub --start (math (string length $commandName) + 1) $commandLineUntilCursor)
+  set -l args (string sub --start (math (string length $commandName) + 1) $commandLineUntilCursor)
 
-  set completions ($commandName --complete=fish $args)
-  #set completions (cat test)
-  echo "NEW" > file
+  set -l cmd "$commandName --complete=fish \"$args\""
+  #echo $cmd > file
+  set -l completions (eval $cmd)
+  #echo "NEW" >> file
   for completion in $completions
     echo $completion
-    echo $completion >> file
+    #echo $completion >> file
   end
 end
 
 complete -c gwhisper -e
-complete -f -c gwhisper -a '(getCompletions)'
+complete -f -c gwhisper -a '(_gwhisper_getCompletions)'
