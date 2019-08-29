123794d66e8a2f8907b5062c11d6f9baae65b456
Taojun <taojun.li@ibm.com>
2019-08-29 17:47:02 +0200

    fix method document
    Signed-off-by: Taojun <taojun.li@ibm.com>

--- src/libCli/GrammarConstruction.cpp
+++ src/libCli/GrammarConstruction.cpp
@@ -351,9 +351,7 @@ class GrammarInjectorMethods : public GrammarInjector
                 for (int i = 0; i < service->method_count(); ++i)
                 {
                     auto childAlt = m_grammar.createElement<FixedString>(service->method(i)->name());
-                    std::string doc = service->method(i)->DebugString();
-                    //->options().DebugString();
-                    std::cout << "have method: " << service->method(i)->name() << " doc=" << doc << std::endl;
+                    std::string doc = service->method(i)->options().DebugString();
                     childAlt->setDocument(OutputFormatter::getOptionString(doc));
                     result->addChild(childAlt);
                 }
