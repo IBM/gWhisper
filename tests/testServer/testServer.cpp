a73f0f6b8144fd339ded44df6aae01d035fa9d53
Anna Riesch <Anna.Riesch@ibm.com>
2021-04-27 13:57:59 +0200

    Included Keys and Certs
    Signed-off-by: Anna Riesch <Anna.Riesch@ibm.com>

--- tests/testServer/testServer.cpp
+++ tests/testServer/testServer.cpp
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include <iostream>
+#include <fstream>
 
 #include <grpcpp/grpcpp.h>
 #include "ServiceScalarTypeRpcs.hpp"
@@ -20,6 +21,26 @@
 #include "ServiceComplexTypeRpcs.hpp"
 #include "ServiceNestedTypeRpcs.hpp"
 #include "ServiceStatusHandling.hpp"
+
+/// Function for reading and returning credentials (Key, Cert) for secure server
+std::string readFromFile(const char *f_path)
+{
+    std::ifstream credFile;
+    credFile.open(f_path);
+    std::string fileContent;
+    char nextChar;
+
+    credFile >> std::noskipws;
+    while (credFile >> nextChar)
+    {
+        fileContent += nextChar;
+    }
+
+    credFile.close();
+
+    std::cout << "File content of" << f_path << ": " << fileContent;
+    return fileContent;
+}
 
 int main(int argc, char **argv)
 {
@@ -52,18 +73,35 @@ int main(int argc, char **argv)
     // std::shared_ptr< grpc::SslServerCredentials > secureCredentials
     // builder.AddListeningPort(addr+":465", creds )
 
-    std::cout << "Starting server listening on " << serverAddr << std::endl;
-    grpc::ServerBuilder builder;
-    builder.AddListeningPort(serverAddr, grpc::InsecureServerCredentials());
+    // std::cout << "Starting server listening on " << serverAddr << std::endl;
+    // grpc::ServerBuilder builder;
+    // builder.AddListeningPort(serverAddr, grpc::InsecureServerCredentials());
 
     std::cout << "Starting secure server listening on " << serverAddr << std::endl;
     // Create a default SSL Credentials object.
+    const char *serverKeyPath = "cert-key-pairs/serverPrivateKey.key";
+    const char *serverCertPath = "cert-key-pairs/serverCert.crt";
+    const char *clientCertPath = "cert-key-pairs/clientCert.crt";
+
     std::shared_ptr<grpc::ServerCredentials> creds;
 
+    // Get Credentials from Files and define them as  Server Key Cert Pair (needed to fill pem_key_cert_pairs vector of SslCredentialOptions)
     // Here we can change to chain of trust instead of selfsigned
-    grpc::SslServerCredentialsOptions::PemKeyCertPair pkcp = {};
-    // TODO: Set the right options
-    creds = grpc::ServerCredentials(grpc::SslCredentialsOptions());
+    std::string serverKey = readFromFile(serverKeyPath);
+    std::string serverCert = readFromFile(serverCertPath);
+    std::string clientCert = readFromFile(clientCertPath);
+
+    grpc::SslServerCredentialsOptions::PemKeyCertPair pkcp = {serverKey, serverCert};
+
+    // Security Options for ssl connection
+    grpc::SslServerCredentialsOptions sslOpts(GRPC_SSL_REQUEST_CLIENT_CERTIFICATE_AND_VERIFY);
+
+    // Set credentials
+    sslOpts.pem_key_cert_pairs.push_back(pkcp);
+    sslOpts.pem_root_certs = clientCert;
+
+    creds = grpc::SslServerCredentials(sslOpts);
+    grpc::ServerBuilder builder;
     builder.AddListeningPort(serverAddr, creds);
 
     // register all services:
