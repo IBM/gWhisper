f5e7bf3e06e97f88abb88bb32497a19471f43c53
Rahman Abber Tahir <rahman.abber.tahir@ibm.com>
2023-11-20 13:17:17 +0100

    always print pimitive fields in json output.
    Signed-off-by: Rahman Abber Tahir <rahman.abber.tahir@ibm.com>

--- src/libCli/MessageFormatterJson.cpp
+++ src/libCli/MessageFormatterJson.cpp
@@ -24,6 +24,10 @@ namespace cli
         std::string resultString;
         google::protobuf::util::JsonPrintOptions printOptions;
         printOptions.preserve_proto_field_names = true;
+
+        // Will print primitive fields regardless of their values. So e.g. an int32 field set to 0 will not be omitted. 
+        printOptions.always_print_primitive_fields = true;
+        
         google::protobuf::util::Status status = google::protobuf::util::MessageToJsonString(f_message, &resultString, printOptions);
         if(not status.ok())
         {
